231f395f3abc064df266602daa789fbaf75569c4
Ubuntu <ubuntu@ip-172-31-6-113.us-east-2.compute.internal>
2020-06-27 23:50:43 +0000

    added start script in package.json and chaged db url to remote

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', 
-    {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    });
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost/todo-api', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+    
+mongoose.connection.on("connected", ()=>{
+	console.log("Mongoose is connected!!!");
+})
+
 mongoose.Promise = Promise;
 module.exports.Todo = require("./todo");
